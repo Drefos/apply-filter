c608e46202ea79f10b83d26fb5993604244d89fd
Maciej Borkowski <maciekbb98@gmail.com>
2021-02-26 21:24:25 +0100

    created gamma filter

---
+++ src/model/functionFilters/GammaFilter.java
@@ -0,0 +1,37 @@
+package model.functionFilters;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import model.Filter;
+
+public class GammaFilter implements Filter {
+
+    public static double GAMMA_FACTOR = 1.1;
+
+    @Override
+    public Image filterImage(Image oldImage) {
+        WritableImage newImage = new WritableImage((int)oldImage.getWidth(), (int)oldImage.getHeight());
+        PixelWriter writer = newImage.getPixelWriter();
+        PixelReader reader =  oldImage.getPixelReader();
+        for(int y=0; y< oldImage.getHeight(); y++) {
+            for(int x=0; x< oldImage.getWidth(); x++) {
+                Color old = reader.getColor(x,y);
+                writer.setColor(x,y, calculateColor(old.getRed(), old.getGreen(), old.getBlue()));
+            }
+        }
+        return newImage;
+    }
+
+    private Color calculateColor(double red, double green, double blue) {
+        return Color.color(adjust(Math.pow(red, GAMMA_FACTOR)), adjust(Math.pow(green, GAMMA_FACTOR)), adjust(Math.pow(blue, GAMMA_FACTOR)));
+    }
+
+    private double adjust(double n) {
+        if(n<0) return 0.0;
+        if(n>1) return 1.0;
+        return n;
+    }
+}
