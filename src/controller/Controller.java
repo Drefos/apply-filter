dd7e4f7906ffd12e465803a76051c9a653cf5535
Maciej Borkowski <maciekbb98@gmail.com>
2021-02-27 17:33:06 +0100

    added fxml components to controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,71 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
 public class Controller {
+
+    @FXML
+    private VBox filters;
+
+    @FXML
+    private MenuItem openMenuItem;
+
+    @FXML
+    private MenuItem saveAsMenuItem;
+
+    @FXML
+    private MenuItem closeMenuItem;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private RadioButton inversionRadioButton;
+
+    @FXML
+    private ToggleGroup function;
+
+    @FXML
+    private RadioButton brightnessRadioButton;
+
+    @FXML
+    private RadioButton contrastRadioButton;
+
+    @FXML
+    private RadioButton gammaRadioButton;
+
+    @FXML
+    private Button functionFilterButton;
+
+    @FXML
+    private RadioButton blurRadioButton;
+
+    @FXML
+    private ToggleGroup convolution;
+
+    @FXML
+    private RadioButton gaussianRadioButton;
+
+    @FXML
+    private RadioButton sharpenRadioButton;
+
+    @FXML
+    private RadioButton edgeRadioButton;
+
+    @FXML
+    private RadioButton embossRadioButton;
+
+    @FXML
+    private Button convolutionFilterButton;
+
+    @FXML
+    public void initialize() {
+    }
+
 }
