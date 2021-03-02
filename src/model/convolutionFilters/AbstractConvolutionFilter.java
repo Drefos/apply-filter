15709b8143ddd45f4b795a6314b2af9bfe9e2747
Maciej Borkowski <maciekbb98@gmail.com>
2021-03-02 10:57:44 +0100

    implemented calculateColor in abstract convolution filter
    separated kernel function

--- src/model/convolutionFilters/AbstractConvolutionFilter.java
+++ src/model/convolutionFilters/AbstractConvolutionFilter.java
@@ -8,14 +8,16 @@ import javafx.scene.paint.Color;
 
 public abstract class AbstractConvolutionFilter {
 
-    protected double[][] kernel;
-    protected int anchorX;
-    protected int anchorY;
+    protected Kernel kernel;
     protected double off;
     protected double divisor;
+    protected int imgWidth;
+    protected int imgHeight;
 
     public Image filterImage(Image oldImage) {
-        WritableImage newImage = new WritableImage((int) oldImage.getWidth(), (int) oldImage.getHeight());
+        this.imgWidth = (int) oldImage.getWidth();
+        this.imgHeight = (int) oldImage.getHeight();
+        WritableImage newImage = new WritableImage(imgWidth, imgHeight);
         PixelWriter writer = newImage.getPixelWriter();
         PixelReader reader = oldImage.getPixelReader();
         for (int y = 0; y < oldImage.getHeight(); y++) {
@@ -26,7 +28,29 @@ public abstract class AbstractConvolutionFilter {
         return newImage;
     }
 
-    protected abstract Color calculateColor(int x, int y, PixelReader reader);
+    protected Color calculateColor(int x, int y, PixelReader reader) {
+        double k, red = 0, green = 0, blue = 0;
+        Color old;
+        for (int i = 0; i < kernel.coefficients.length; i++) {
+            for (int j = 0; j < kernel.coefficients[i].length; j++) {
+                old = reader.getColor(bound(x + i - kernel.anchorX, 0, imgWidth - 1), bound(y + j - kernel.anchorY, 0, imgHeight - 1));
+                k = kernel.coefficients[i][j];
+                red += k * old.getRed();
+                green += k * old.getGreen();
+                blue += k * old.getBlue();
+            }
+        }
+        red = red / divisor + off;
+        green = green / divisor + off;
+        blue = blue / divisor + off;
+        return Color.color(adjust(red), adjust(green), adjust(blue));
+    }
+
+    protected int bound(int n, int min, int max) {
+        if (n < min) return min;
+        if (n > max) return max;
+        return n;
+    }
 
     protected double adjust(double n) {
         if (n < 0) return 0.0;
