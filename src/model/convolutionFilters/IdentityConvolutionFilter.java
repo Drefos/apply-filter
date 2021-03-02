e7eb2b895c889bc9f98ad82212124c40d6bfa57f
Maciej Borkowski <maciekbb98@gmail.com>
2021-03-02 11:01:41 +0100

    creted IdentityConvolutionFilter

---
+++ src/model/convolutionFilters/IdentityConvolutionFilter.java
@@ -0,0 +1,17 @@
+package model.convolutionFilters;
+
+public class IdentityConvolutionFilter extends AbstractConvolutionFilter {
+
+    public IdentityConvolutionFilter() {
+        kernel = new Kernel();
+        kernel.coefficients = new double[][]{
+                {0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0}};
+        kernel.anchorX = 1;
+        kernel.anchorY = 1;
+        off = 0.0;
+        divisor = 9.0;
+    }
+}
+
